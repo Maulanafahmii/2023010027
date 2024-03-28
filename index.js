33cf93a129afec1970c2b3fa15003faea03c8d7a
Maulana Fahmi <maulanafahmii916@gmail.com>
2024-03-28 10:40:26 +0700

    latihan1

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 <html lang="en">
+
 <head>
-<title>Belajar Javascript document.write</title>
+    <title>Belajar Javascript</title>
 </head>
 <body>
-<h1>Hello World!</h1>
-<script>
-document.write("<hr>");
-document.write("<p>Lagi belajar JavaScript nih</p>");
-document.write("di <a
-href='https://blog.xsis.academy'>Xsis Academy</a>")
+    <h1>Tutorial Javascript untuk Pemula</h1>
+    <div id="hasil-output-1"></div>
+    <hr>
+    <div id="hasil-output-2"></div>
+<script> 
+    // membuat objek elemen
+    var hasil = document.getElementById("hasil-output-1");
+    // menampilkan output ke elemen hasil
+    hasil.innerHTML = "<p>Contoh menggunakan InnerHTML
+1</p>";
+    //atau bisa juga langsung seperti ini
+    document.getElementById("hasil-output-2").innerHTML =
+`<h1>Hello Winners!</h1>
+<div>Ini adalah tutorial javascript dasar, contoh menggunakan
+innerHTML. untuk tutorial lebih banyak silahkan lihat
+<a href="https://polibang.ac.id" target="_blank"
+rel="noopener noreferrer">disini</a>.
+    </div>`;
 </script>
-<!-- Menggunakan document.write() setelah dokumen HTML
-dimuat, akan menghapus semua HTML yang ada contohnya dibawah -->
-<hr>
-<button type="button"
-onclick="document.write('Ini contoh menggunakan
-document.write() setelah dokumen HTML dimuat, akan menghapus
-semua HTML yang ada')">Coba
-deh!</button>
 </body>
 </html>
