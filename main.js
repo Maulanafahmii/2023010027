969f0b154ae2260b476c01619813ede914ecbd6c
Maulana Fahmi <maulanafahmii916@gmail.com>
2024-05-15 19:47:49 +0700

    feat

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log(“Hello, World!”);
+alert(Hello world)
+
+prompt(Masukan usia anda? )
+let usia = 20
+usia = 18
+console log(' Hallo Fahmi usia kamu adalah '+ usia)
+
+alert (' Usia kamu adalah'+ usia)
